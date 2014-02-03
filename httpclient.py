a4207273baba2c81bf73a994efe06d273ec135bd
Olexiy Berjanskii <berjansk@cs.ualberta.ca>
2014-02-03 00:30:21 -0700

    GET done

--- httpclient.py
+++ httpclient.py
@@ -23,24 +23,31 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+from urlparse import urlparse
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPRequest(object):
     def __init__(self, code=200, body=""):
-        self.code = code
+        self.code = int(code)
         self.body = body
 
 class HTTPClient(object):
     #def get_host_port(self,url):
 
-    def connect(self, host, port):
+    def connect(self, host, port="80"):
         # use sockets!
-        return None
+        if(port == None):
+            port = 80
+        print(host, port)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))  
+        return s
 
     def get_code(self, data):
-        return None
+        parts = data.split()
+        return parts[1]
 
     def get_headers(self,data):
         return None
@@ -61,8 +68,43 @@ class HTTPClient(object):
         return str(buffer)
 
     def GET(self, url, args=None):
-        code = 500
+        parsed_url = urlparse(url)
+        s = self.connect(parsed_url.hostname, parsed_url.port)
+        print(parsed_url)
+
+        path = parsed_url.path
+        if(path == ""):
+            path = "/"
+        if(parsed_url.query != ""):
+            path += "?" + parsed_url.query
+
+        request = "GET " + path + " HTTP/1.1\r\n"
+        request += "Host: " + parsed_url.hostname + "\r\n"
+        request += "Accept: */*" + "\r\n" 
+        request += "User-Agent: Custom\r\n"
+        request += "Connection: close\r\n\r\n"
+
+        print ("\n\n" + request + "\n\n")
+        s.send(request)
+        response = self.recvall(s)
+
+        response_arr = response.splitlines()
+
+        code = None
+        reach_content = False
         body = ""
+        for i, line in enumerate(response_arr):
+            if(i == 0):
+                code = self.get_code(line)
+            elif(reach_content == False):
+                self.get_headers(line)
+                if(line == ""):
+                    reach_content = True
+            else:
+                body += line + "\n"
+
+        print "\n\n###########GOT RESPONSE" + code + " " + body+"\n############\n\n"
+
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
@@ -70,7 +112,8 @@ class HTTPClient(object):
         body = ""
         return HTTPRequest(code, body)
 
-    def command(self, url, command="GET", args=None):
+    def command(self, command, url, args=None):
+        print("COMMAND" , url, command)
         if (command == "POST"):
             return self.POST( url, args )
         else:
@@ -85,4 +128,4 @@ if __name__ == "__main__":
     elif (len(sys.argv) == 3):
         print client.command( sys.argv[1], sys.argv[2] )
     else:
-        print client.command( command, sys.argv[1] )    
+        print client.command( sys.argv[1], sys.argv[2], sys.argv[3] )    
